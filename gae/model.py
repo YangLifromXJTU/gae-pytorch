71037399318747bd85171a0adb322e6a6ce1202e
YoungLeefromXJTU <674985241@qq.com>
2021-01-28 22:26:15 +0800

    dont know

--- gae/model.py
+++ gae/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from gae.layers import GraphConvolution
+from layers import GraphConvolution
 
 
 class GCNModelVAE(nn.Module):
@@ -29,6 +29,22 @@ class GCNModelVAE(nn.Module):
         mu, logvar = self.encode(x, adj)
         z = self.reparameterize(mu, logvar)
         return self.dc(z), mu, logvar
+
+class GCNModelAE(nn.Module):
+    def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
+        super(GCNModelAE, self).__init__()
+        self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
+        self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+    
+    def encode(self, x, adj):
+        hidden1 = self.gc1(x, adj)
+        return self.gc2(hidden1, adj), None
+    
+    def forward(self, x, adj):
+        hidden1 = self.gc1(x, adj)
+        output = self.gc2(hidden1, adj)
+        return self.dc(output), None, None
 
 
 class InnerProductDecoder(nn.Module):
